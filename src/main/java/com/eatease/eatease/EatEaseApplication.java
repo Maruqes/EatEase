9e142b2ee32aae947aa638dba5e9b2107069c992
Maruqes <m4rques1s0n@gmail.com>
2025-05-29 00:32:59 +0100

    datetime error

--- src/main/java/com/eatease/eatease/EatEaseApplication.java
+++ src/main/java/com/eatease/eatease/EatEaseApplication.java
@@ -1,5 +1,7 @@
 package com.eatease.eatease;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,9 +10,9 @@ import org.springframework.context.ApplicationContext;
 public class EatEaseApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Lisbon"));
 		ApplicationContext context = SpringApplication.run(EatEaseApplication.class, args);
 		Extra test = context.getBean(Extra.class);
 		test.criar();
-		//hello world
 	}
 }
